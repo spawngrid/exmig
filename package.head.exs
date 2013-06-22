1d8bc576e8539159e46e74dcf3443ebd4a443987
Yurii Rashkovskii <yrashk@spawngrid.com>
2013-06-22 18:56:44 +0100

    Add expm package

---
+++ package.head.exs
@@ -0,0 +1,5 @@
+Expm.Package.new(name: "exmig", description: "Elixir DSL for migrations",
+                 version: :head, keywords: ["db","database","migration"],
+                 maintainers: [[name: "Yurii Rashkovskii",
+                                email: "yrashk@spawngrid.com"]],
+                 repositories: [[github: "spawngrid/exmig"]])
