4fe4028acecf1c2ad6e97844abd6e02b648af7ab
Yurii Rashkovskii <yrashk@spawngrid.com>
2013-06-29 17:22:58 +0200

    Make migrations table name configurable in Migrations.DBI

--- lib/implementation/dbi.ex
+++ lib/implementation/dbi.ex
@@ -1,20 +1,20 @@
-defrecord Migrations.DBI, for: nil
+defrecord Migrations.DBI, for: nil, table: "migrations"
 
 defprotocol Migration.DBI.SQL do
   @type t
   @type result :: {DBI.statement, DBI.bindings}
 
-  @spec init(t) :: result
-  def init(t)
+  @spec init(t, Migrations.DBI.t) :: result
+  def init(t, dbi)
 
-  @spec list(t) :: result
-  def list(t)
+  @spec list(t, Migrations.DBI.t) :: result
+  def list(t, dbi)
 
-  @spec add(t, Migrations.Migration.t) :: result
-  def add(t, migration)
+  @spec add(t, Migrations.Migration.t, Migrations.DBI.t) :: result
+  def add(t, migration, dbi)
 
-  @spec remove(t, Migrations.Migration.t) :: result
-  def remove(t, migration)
+  @spec remove(t, Migrations.Migration.t, Migrations.DBI.t) :: result
+  def remove(t, migration, dbi)
 end
 
 defimpl Migrations.Implementation, for: Migrations.DBI do
@@ -22,27 +22,27 @@ defimpl Migrations.Implementation, for: Migrations.DBI do
   alias Migrations.DBI, as: T
 
   def init(T[for: for] = t) do
-    {query, bindings} = Migration.DBI.SQL.init(for)
+    {query, bindings} = Migration.DBI.SQL.init(for, t)
     DBI.query!(for, query, bindings)
     t
   end
 
-  def migrations(T[for: for]) do
-    {query, bindings} = Migration.DBI.SQL.list(for)
+  def migrations(T[for: for] = t) do
+    {query, bindings} = Migration.DBI.SQL.list(for, t)
     migrations = DBI.query!(for, query, bindings)
     lc {id, ts} inlist Enum.to_list(migrations) do
       Migrations.Migration.new(id: id, timestamp: ts)
     end
   end
 
-  def add!(T[for: for], migration) do
-    {query, bindings} = Migration.DBI.SQL.add(for, migration)
+  def add!(T[for: for] = t, migration) do
+    {query, bindings} = Migration.DBI.SQL.add(for, migration, t)
     DBI.Result[count: 1] = DBI.query!(for, query, bindings)
     :ok
   end
 
-  def remove!(T[for: for], migration) do
-    {query, bindings} = Migration.DBI.SQL.remove(for, migration)
+  def remove!(T[for: for] = t, migration) do
+    {query, bindings} = Migration.DBI.SQL.remove(for, migration, t)
     DBI.query!(for, query, bindings)
     :ok
   end
--- lib/implementation/dbi/pgsql.ex
+++ lib/implementation/dbi/pgsql.ex
@@ -1,24 +1,24 @@
 defimpl Migration.DBI.SQL, for: DBI.PostgreSQL do
 
-  def init(_) do
+  def init(_, Migrations.DBI[table: table]) do
     {""", []}
-       CREATE TABLE IF NOT EXISTS migrations (
+       CREATE TABLE IF NOT EXISTS #{table} (
          id VARCHAR(255) PRIMARY KEY,
          ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP
        )
     """
   end
 
-  def list(_) do
-    {"SELECT id, ts FROM migrations ORDER BY id DESC", []}
+  def list(_, Migrations.DBI[table: table]) do
+    {"SELECT id, ts FROM #{table} ORDER BY id DESC", []}
   end
 
-  def add(_, Migrations.Migration[id: id]) do
-    {"INSERT INTO migrations (id) VALUES (:{id})",[id: id]}
+  def add(_, Migrations.Migration[id: id], Migrations.DBI[table: table]) do
+    {"INSERT INTO #{table} (id) VALUES (:{id})",[id: id]}
   end
 
-  def remove(_, Migrations.Migration[id: id]) do
-    {"DELETE FROM migrations WHERE id = :{id}",[id: id]}
+  def remove(_, Migrations.Migration[id: id], Migrations.DBI[table: table]) do
+    {"DELETE FROM #{table} WHERE id = :{id}",[id: id]}
   end
 
 
